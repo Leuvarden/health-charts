b269bedfdbfeefc58b0034acd778485ac6bd0ba9
Maryna Leanovich <sollferino@gmail.com>
2016-10-04 02:14:22 +0300

    .-.

--- js/index.js
+++ js/index.js
@@ -1,144 +1,235 @@
 $(document).ready(function() {
 
-	//page animation
+    $('.connect').on('click', function () {
+    	$('.side-menu').toggleClass("invisible");
 
-	var zTop = 1;
-	$('a').on('click',function(e){
- 		e.preventDefault();
- 		// Animate up or down based on DOM index
- 		var index = $($(this).attr('href')).index();
- 		var wh = $(window).height();
- 		$('section').each(function(){
- 			var $tgt = $(this);
- 			var idx = $tgt.index();
- 			$tgt.removeClass();
- 			if (index == idx) {
-				$tgt.stop(true,true).animate({top:0},'linear').addClass('active').css({'z-index':zTop});
-			}
- 			if (index < idx) {
-	 			$tgt.stop(true,true).animate({top:wh},'linear').addClass('below');
- 			}
- 			if(index > idx ){
-	 		$tgt.stop(true,true).animate({top:-wh},'linear').addClass('above');
- 			}
- 		});
- 	zTop++;
-	})
+    });
 
-	$('a[href="#a"]').trigger('click');
+    var elm = document.querySelector('#main-header');
+    var ms = new MenuSpy(elm);
 
-	$(window).on('resize',function(){
- 	var wh = $(window).height();
- 	$('article, section').height(wh);
- 	$('.active').css({top:0});
- 	$('.above').css({top:-wh});
- 	$('.below').css({top:wh});
-	}).trigger('resize');
+    //graph's properties
 
-	//graph's properties
-
-	$('#steps').highcharts({
-		chart: {
-			type: 'areaspline',
-				backgroundColor: 'transparent',
-				spacingBottom: 0,
-        spacingTop: 0,
-        spacingLeft: 0,
-        spacingRight: 0
-		},
-		colors: ['#ff8200','#b0b0b0' ],
-		title: {
-				text: ''
-		},
-		legend: {
-				enabled: false
-		},
-		xAxis: {
-			labels: {
-							enabled: false
-					},
-					tickColor: 'transparent',
-					gridLineColor: 'transparent'
-		},
-		yAxis: {
-			title: '',
-			labels: {
-							enabled: false
-					},
-					gridLineColor: 'transparent',
-					lineColor: 'transparent'
-		},
-		tooltip: {
-				shared: true,
-				valueSuffix: 'k steps'
-		},
-		credits: {
-				enabled: false
-		},
-		plotOptions: {
-				areaspline: {
-						fillOpacity: 0.5
-				}
-		},
-		series: [{
-				name: 'John',
-				data: [3, 4, 3, 5, 4, 10, 12]
-		}]
-});
-
-$('#battery').highcharts({
+    $('#steps').highcharts({
         chart: {
-            type: 'column',
-						backgroundColor: 'transparent',
-						spacingBottom: 0,
-		        spacingTop: 0,
-		        spacingLeft: 0,
-		        spacingRight: 0
+            type: 'areaspline',
+            backgroundColor: 'transparent',
+            spacingBottom: 0,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+            margin: [0, 0, 0, 0]
         },
+        colors: ['#ef6c00', '#b0b0b0'],
         title: {
             text: ''
         },
-        xAxis: {
-            labels: {
-							enabled: false
-					},
-					tickColor: 'transparent',
-					gridLineColor: 'transparent',
-					lineColor: 'transparent'
-        },
-        yAxis: {
-            labels: {
-							enabled: false
-					},
-					gridLineColor: 'transparent',
-					title: ''
-		},
         legend: {
             enabled: false
         },
+        xAxis: {
+            labels: {
+                enabled: false
+            },
+            tickColor: 'transparent',
+            gridLineColor: 'transparent',
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+                month: '%e. %b',
+                year: ''
+            },
+        },
+        yAxis: {
+            title: '',
+            labels: {
+                enabled: false
+            },
+            gridLineColor: 'transparent',
+            lineColor: 'transparent'
+        },
         tooltip: {
+            shared: true,
+            valueSuffix: 'k steps'
+        },
+        credits: {
             enabled: false
         },
-				credits: {
-						enabled: false
-				},
         plotOptions: {
-            column: {
-                stacking: 'normal',
-                dataLabels: {
-                    enabled: false
-                }
+            areaspline: {
+                fillOpacity: 0.5
             }
         },
         series: [{
             name: 'John',
-            data: [5]
-        }, {
-            name: 'Jane',
-            data: [5]
+            data: [3, 4, 3, 5, 4, 10, 12]
         }]
     });
 
+    $('#heart').highcharts({
+        chart: {
+            type: 'areaspline',
+            backgroundColor: 'transparent',
+            spacingBottom: 0,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+            margin: [0, 0, 0, 0]
+        },
+        colors: ['#fa5252'],
+        title: {
+            text: ''
+        },
+        legend: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            },
+            tickColor: 'transparent',
+            gridLineColor: 'transparent',
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+                month: '%e. %b',
+                year: ''
+            },
+        },
+        yAxis: {
+            title: '',
+            labels: {
+                enabled: false
+            },
+            gridLineColor: 'transparent',
+            lineColor: 'transparent'
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: 'k steps'
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [3, 4, 3, 5, 4, 10, 12]
+        }]
+    });
 
+    $('#sleep').highcharts({
+        chart: {
+            type: 'areaspline',
+            backgroundColor: 'transparent',
+            spacingBottom: 0,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+            margin: [0, 0, 0, 0]
+        },
+        colors: ['#15aabf'],
+        title: {
+            text: ''
+        },
+        legend: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            },
+            tickColor: 'transparent',
+            gridLineColor: 'transparent',
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+                month: '%e. %b',
+                year: ''
+            },
+        },
+        yAxis: {
+            title: '',
+            labels: {
+                enabled: false
+            },
+            gridLineColor: 'transparent',
+            lineColor: 'transparent'
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: 'k steps'
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [3, 4, 3, 5, 4, 10, 12]
+        }]
+    });
 
-	});
+    $('#battery').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false,
+            backgroundColor: 'transparent',
+            spacingBottom: 0,
+            spacingTop: 0,
+            spacingLeft: 0,
+            spacingRight: 0,
+            margin: [0, 0, 0, 0]
+
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            enabled: false
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: false,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%'],
+                series: {
+                    states: {
+                        hover: {
+                            enabled: false
+                        }
+                    }
+                }
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Browser share',
+            borderColor: 'transparent',
+            borderWidth: 0,
+            innerSize: '70%',
+            data: [
+                ['', 70],
+                ['', 30]
+            ]
+        }]
+    });
+
+});
