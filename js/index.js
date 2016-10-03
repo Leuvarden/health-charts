c844cc5b9ca32559d33775121b3d37524ca3634c
Maryna Leanovich <sollferino@gmail.com>
2016-10-04 02:23:33 +0300

    add animation

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
 
+  //animation
+  $('a[href^="#"]').click(function(){
+    var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+    return false;
+  });
+
+  //sidebar hiding
     $('.connect').on('click', function () {
     	$('.side-menu').toggleClass("invisible");
 
     });
-
+    //sidebar initializing
     var elm = document.querySelector('#main-header');
     var ms = new MenuSpy(elm);
 
     //graph's properties
-
     $('#steps').highcharts({
         chart: {
             type: 'areaspline',
